d903f37eb69bed8bf0951db10dadc4ad990caa39
gchronis <gigichronis@gmail.com>
2018-10-11 16:33:59 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Twitter Bot
+___
+Simple Twitter bot using Tweepy and Python.
+
+Generates Boggle Boards
+
+Tweepy is a Python library which provide access to Twitter API.
+## Installation 
+``pip install tweepy``
+## Usage
+Before running, please edit config file according to your **[Keys and access token](https://dev.twitter.com/oauth/overview/application-owner-access-tokens)**.
+
+``python3 update_status_bot.py``
+
+Goto your Twitter account profile you can see result.
+
+___
+Details on bot creation [here] (https://medium.com/@frfahim/create-a-simple-twitter-bot-with-python-and-tweepy-60ff5d4d3ad9#.9n0xy5fjd "Medium blog")
---
+++ main.py
@@ -0,0 +1,66 @@
+from config import *
+import tweepy
+import random
+
+def login():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
+
+def tweet(api, string):
+    api.update_status(status=string)
+
+# returns a list of 16 boggle letters
+def generate_board():
+    dice = [
+        "RIFOBX",
+        "IFEHEY",
+        "DENOWS",
+        "UTOKND",
+        "HMSRAO",
+        "LUPETS",
+        "ACITOA",
+        "YLGKUE",
+        "QBMJOA",
+        "EHISPN",
+        "VETIGN",
+        "BALIYT",
+        "EZAVND",
+        "RALESC",
+        "UWILRG",
+        "PACEMD",
+    ]
+    print len(dice)
+    letters = [ random.choice(die) for die in dice ]
+    print len(letters)
+    random.shuffle(letters)
+    print len(letters)
+    return letters
+
+def board_string(letters):
+    horiz = "+---------------+"
+
+    board = ""
+    board += horiz
+    board += "\n"
+    for i in range(4):
+        board += "|"
+        for j in range(4):
+            board += " "
+            letter = letters.pop()
+            if letter == "Q":
+                board += "Qu|"
+            else:
+                board += letter + " |"
+        board += "\n"
+        if not i  == 3:
+            board += "| - - - - - - - |"
+            board += "\n"
+    board += horiz 
+    print board
+
+letters = generate_board()
+boggle_board = board_string(letters)
+api = login()
+api.tweet(boggle_board)
+print string
